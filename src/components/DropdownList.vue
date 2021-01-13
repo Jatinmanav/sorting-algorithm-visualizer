54d363649ddca22c4b08c90c4ad1d16c507c0359
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2021-01-13 23:48:06 +0530

    update header

--- src/components/DropdownList.vue
+++ src/components/DropdownList.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="dropdown-container">Dropdown Menu</div>
+  <div class="dropdown-container">
+    <div class="dropdown-display" @click="toggleDropdown">
+      {{ selected }}
+    </div>
+    <div class="dropdown-items-container" v-if="open">
+      <div
+        class="dropdown-item"
+        v-bind:key="algorithm.id"
+        v-for="algorithm in algorithms"
+      >
+        {{ algorithm.name }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'DropdownList',
+  data() {
+    return {
+      open: false,
+      selected: 'Bubble Sort',
+      algorithms: [
+        { id: 1, name: 'Bubble Sort' },
+        { id: 2, name: 'Quick Sort' },
+        { id: 3, name: 'Merge Sort' },
+      ],
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      console.log('Clicked');
+      this.open = !this.open;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .dropdown-container {
-  display: inline;
+  background: chartreuse;
+}
+
+.dropdown-items-container {
+  position: absolute;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="header">
-    <DropdownList />
-    <button>Visualize</button>
-    <button>Shuffle</button>
-    <label> Speed </label><input type="range" />
+    <div class="logo">SAVisualizer</div>
+    <div class="sorting-options">
+      <DropdownList />
+      <button class="header-button">Visualize</button>
+      <button class="header-button">Shuffle</button>
+      <label> Speed </label>
+      <input type="range" />
+    </div>
   </div>
 </template>
 
@@ -24,6 +28,18 @@ export default {
 <style scoped lang="scss">
 .header {
   background-color: red;
-  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 1em 5%;
+}
+
+.sorting-options {
+  display: flex;
+}
+
+.header-button {
+  outline: none;
+  border: 1px blue solid;
+  background-color: green;
 }
 </style>
